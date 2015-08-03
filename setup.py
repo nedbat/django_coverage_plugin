cc609f36761cbf4278983f929acb3a1bf04e2848
Ned Batchelder <ned@nedbatchelder.com>
2015-08-02 22:04:24 -0400

    0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django_coverage_plugin',
-    version='0.5.5',
+    version='0.6',
     description='Django template coverage.py plugin',
     author='Ned Batchelder',
     author_email='ned@nedbatchelder.com',
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         # If you change this, update tox.ini and requirements.txt also.
         'Django >= 1.4',
-        'coverage >= 4.0a7',
+        'coverage >= 4.0b1',
         'six >= 1.4.0',
     ],
 )
