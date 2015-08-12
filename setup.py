daa36ced1da58cb36de8c12d3e26e34008afae1d
Ned Batchelder <ned@nedbatchelder.com>
2015-08-11 22:12:51 -0400

    Let's call this 1.0b1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django_coverage_plugin',
-    version='0.6',
+    version='1.0b1',
     description='Django template coverage.py plugin',
     author='Ned Batchelder',
     author_email='ned@nedbatchelder.com',
