22994c3f199856b65d25911320a090aafbf826e4
Pamela McA’Nulty <pamela@mcanulty.org>
2017-11-29 23:23:57 -0500

    Add a couple of new tests to test check_debug startup

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,22 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/nedbat/django_coverage_plugin/blob/master/NOTICE.txt
 
-"""Settings tests for django_coverage_plugin."""
+"""
+    Full integration tests for django_coverage_plugin.
+    The tests in this module instantiate a minimal but complete
+    Django project and run tests against it.  This allows for
+    testing against various startup conditions since in other tests,
+    the django startup procedure is only executes once for each
+    python/django version combination.
+"""
 
 import os
-import subprocess
 import shutil
+import subprocess
 
 from .plugin_test import DjangoPluginTestCase
 
 import django
-
 
 VIEW_FUNC_TEXT = """
 def target_view(request):
@@ -20,6 +26,7 @@ def target_view(request):
         {"varA": 1212, "varB": "DcDc"},
     )
 """
+
 TEMPLATE_FILE_TEXT = """
 {% load test_tags %}
 
@@ -54,6 +61,7 @@ ignore_errors = True
 [combine]
 ignore_errors = True
 """
+
 TEST_VIEWS_FILE_TEXT = """
 import django
 from django.test import TestCase
@@ -87,18 +95,40 @@ def lower(value):
 
 
 def change_elem(data, elem_name, sep, func):
+    """
+    Find the configuration definition in `data`, evaluate
+    that defnition, pass the value to func, and convert the output
+    into the expected text format.
+
+    :param data:  Block of text
+    :param elem_name:  Name of element to be changed.  Must be defined
+    at the first column of the data text
+    :param sep:  separator to use in the definition
+    :param func: function to be called for transformation
+    :return: updated data text
+    """
     sep_open, sep_close = {"]": ("[", "]"), ")": ("(", ")")}[sep]
     before, after = data.split("%s = %s" % (elem_name, sep_open), 1)
+
+    # Find the end of the value definition, note that the closing
+    # separator must always be on its own line
     middle, after = after.split("\n%s\n" % sep_close, 1)
+
+    # Put the separators back
     elem = "%s%s%s" % (sep_open, middle, sep_close)
+
+    # Use eval to convert the text of value into a python data object
     elem = eval(elem)
 
+    # Call func to transform the python data object
     elem = func(elem)
 
+    # use pprint to convert the data to text and put it all back together
     import pprint
     middle = pprint.pformat(elem)
     middle = "%s\n%s\n" % (middle[:-1], sep)
-    return "%s%s = %s%s" % (before, elem_name, middle, after)
+    data = "%s%s = %s%s" % (before, elem_name, middle, after)
+    return data
 
 
 class IntegrationTest(DjangoPluginTestCase):
@@ -132,16 +162,16 @@ class IntegrationTest(DjangoPluginTestCase):
             fmt = "OS Error:\n  cmd: %s\n  exc: %s\ncwd: %s\n"
             raise Exception(fmt % (args, e, self.cwd))
 
-    def _pycmd(self, *args, **kwargs):
-        args = [self.python] + list(args)
-        return self._cmd(*args, **kwargs)
-
     def _cmd_global(self, *args, **kwargs):
+        """
+        Appends the env_bin path to the executable in arg[0]
+        Primarily used to run the `coverage` command
+        """
         args = list(args)
         args[0] = os.path.join(self.env_bin, args[0])
         return self._cmd(*args, **kwargs)
 
-    def _start_project(self, project_name):
+    def _start_project(self, project_name, debug=True):
         self.project_dir = os.path.join(os.getcwd(), project_name)
         if os.path.exists(self.project_dir):
             shutil.rmtree(self.project_dir)
@@ -149,16 +179,12 @@ class IntegrationTest(DjangoPluginTestCase):
         self.assertFalse(output)
         self.settings_file = os.path.join(self.project_dir, project_name, "settings.py")
         self._add_installed_app("django_coverage_plugin")
-        # self.addCleanup(shutil.rmtree, self.project_dir)
+        self._update_templates_config(debug=debug)
+        # Comment this line out to view the generated project files
+        self.addCleanup(shutil.rmtree, self.project_dir)
         return output
 
     def _add_installed_app(self, app_name):
-        with open(self.settings_file) as f:
-            settings_data = f.read()
-
-        # -- Add app to installed apps
-        sep = ")" if django.VERSION < (1, 9) else "]"
-
         def _add_app(apps):
             if django.VERSION < (1, 9):
                 apps = list(apps)
@@ -166,24 +192,27 @@ class IntegrationTest(DjangoPluginTestCase):
             if django.VERSION < (1, 9):
                 apps = tuple(apps)
             return apps
-        settings_data = change_elem(settings_data, "INSTALLED_APPS", sep, _add_app)
 
-        def _update_templates_config(templates_config):
+        with open(self.settings_file) as f:
+            settings_data = f.read()
+        sep = ")" if django.VERSION < (1, 9) else "]"
+        settings_data = change_elem(settings_data, "INSTALLED_APPS", sep, _add_app)
+        self._save_py_file(self.settings_file, settings_data)
+
+    def _update_templates_config(self, debug=True):
+        def _update_config(templates_config):
             if "APP_DIRS" in templates_config[0]:
                 del templates_config[0]['APP_DIRS']
-            templates_config[0]['OPTIONS']['debug'] = True
+            templates_config[0]['OPTIONS']['debug'] = debug
             templates_config[0]['OPTIONS']['loaders'] = (
                 'django.template.loaders.app_directories.Loader',
             )
             return templates_config
 
-        settings_data = change_elem(settings_data, "TEMPLATES", "]", _update_templates_config)
+        with open(self.settings_file) as f:
+            settings_data = f.read()
+        settings_data = change_elem(settings_data, "TEMPLATES", "]", _update_config)
         self._save_py_file(self.settings_file, settings_data)
-
-    def _print_file(self, file_data, file_name):
-        print("\n-------- begin: %s  --------" % file_name)
-        print(file_data)
-        print("-------- end: %s  --------\n\n" % file_name)
 
     def _save_py_file(self, path, data, show_data=False):
         with open(path, "w") as f:
@@ -201,8 +230,15 @@ class IntegrationTest(DjangoPluginTestCase):
     def _run_coverage(self, *args):
         self.set_cwd(self.project_dir)
         output = self._cmd_global("coverage", "run", "--rcfile", self.config_file, *args)
-        print(output)
-        self.assertNotIn("Disabling plugin", output)
+
+        if 0:
+            print("----- start output ----")
+            print(output)
+            print("------ end output ------")
+
+        if "Disabling plugin" in output:
+            return output, None
+
         env = os.environ.copy()
         env['DJANGO_SETTINGS_MODULE'] = self.settings_file
         coverage_report = self._cmd_global(
@@ -212,9 +248,9 @@ class IntegrationTest(DjangoPluginTestCase):
         coverage_report = self.parse_coverage_report(coverage_report)
         return output, coverage_report
 
-    def _create_django_project(self, project_name, app_name):
+    def _create_django_project(self, project_name, app_name, debug):
         self.cwd = os.getcwd()
-        self._start_project(project_name)
+        self._start_project(project_name, debug)
         self._start_app(app_name)
         self.config_file = self._add_project_file(COVERAGE_CFG_FILE_TEXT, "coverage.cfg")
         self.template_file = self._add_project_file(
@@ -228,12 +264,12 @@ class IntegrationTest(DjangoPluginTestCase):
             import_reverse = "from django.urls import reverse"
 
         test_views_text = TEST_VIEWS_FILE_TEXT % locals()
-        self.test_views_file = self._add_project_file(test_views_text, app_name, "test_views.py")
+        self._add_project_file(test_views_text, app_name, "test_views.py")
 
         self.views_file = os.path.join(self.project_dir, app_name, "views.py")
         self.urls_file = os.path.join(self.project_dir, project_name, "urls.py")
-        self.test_views_file = self._add_project_file("", app_name, "templatetags", "__init__.py")
-        self.test_views_file = self._add_project_file(
+        self._add_project_file("", app_name, "templatetags", "__init__.py")
+        self._add_project_file(
             TEMPLATE_TAG_TEXT, app_name, "templatetags", "test_tags.py"
         )
 
@@ -275,10 +311,66 @@ class IntegrationTest(DjangoPluginTestCase):
 
         self._save_py_file(self.urls_file, urls_data)
 
+    def test_no_debug(self):
+        self._create_django_project("integration_template_render", "app_template_render", debug=False,)
+        output, coverage_report = self._run_coverage("manage.py", "test", "app_template_render")
+        self.assertIn("Disabling plugin", output)
+        self.assertIn("Template debugging must be enabled in settings.", output)
+
+    def test_wrong_engine(self):
+        self._create_django_project("integration_template_render", "app_template_render", debug=True)
+        self._change_template_engine()
+        output, coverage_report = self._run_coverage("manage.py", "test", "app_template_render")
+        self.assertIn("Disabling plugin", output)
+        self.assertIn("Can't use non-Django templates.", output)
+
+    def _change_template_engine(self):
+        with open(self.settings_file) as f:
+            settings_data = f.read()
+        settings_data = settings_data.replace(
+            "django.template.backends.django.DjangoTemplates",
+            "integration_template_render.settings.FakeTemplateBackend",
+        )
+        settings_data = settings_data.replace(
+            "\nTEMPLATES = ",
+            """
+from django.template import TemplateDoesNotExist, TemplateSyntaxError
+from django.template.backends.base import BaseEngine
+from django.template.backends.utils import csrf_input_lazy, csrf_token_lazy
+
+class Template(object):
+    def __init__(self, template):
+        self.template = template
+    def render(self, context=None, request=None):
+        return "1212 DcDc LOWER: dcdc <title>Example: 1212</title>"
+class FakeTemplateBackend(BaseEngine):
+    app_dirname = 'fake'
+
+    def __init__(self, params):
+        params = params.copy()
+        params.pop('OPTIONS')
+        super(FakeTemplateBackend, self).__init__(params)
+
+    def from_string(self, template_code):
+        return Template(template_code)
+
+    def get_template(self, template_name):
+        return Template(template_name)
+
+TEMPLATES = """,
+        )
+        self._save_py_file(self.settings_file, settings_data)
+
     def test_template_render(self):
-        self._create_django_project("integration_template_render", "app_template_render")
+        self._create_django_project(
+            "integration_template_render",
+            "app_template_render",
+            debug=True,
+        )
 
         output, coverage_report = self._run_coverage("manage.py", "test", "app_template_render")
+
+        self.assertNotIn("Disabling plugin", output)
         self.assertIn("\nOK\n", output)
         self.assertIn("Ran 1 test", output)
         self.assertNotIn("ERROR", output)
@@ -289,15 +381,16 @@ class IntegrationTest(DjangoPluginTestCase):
         self.assertIsCovered(coverage_report, "integration_template_render/__init__.py")
         self.assertIsCovered(coverage_report, "integration_template_render/urls.py")
 
+        # Different versions of Django result in different coverage for manage.py
         if django.VERSION < (1, 10):
             expect_missing, expect_pct = 0, 100
         elif django.VERSION < (2, 0):
             expect_missing, expect_pct = 6, 54
         else:
-            # This is django-tip, so it's likely to change over time.
+            # This includes django-tip, so it's likely to change over time.
             expect_missing, expect_pct = 2, 78
-
         self.assertIsCovered(coverage_report, "manage.py", expect_missing, expect_pct)
+
         self.assertIsCovered(coverage_report, "app_template_render/__init__.py")
         self.assertIsCovered(coverage_report, "app_template_render/views.py")
         self.assertIsCovered(coverage_report, "app_template_render/templates/target_template.html")
@@ -343,7 +436,7 @@ class IntegrationTest(DjangoPluginTestCase):
                         continue
                     elif "-" in chunk:
                         start, end = chunk.split("-", 1)
-                        for i in range(int(start), int(end+1)):
+                        for i in range(int(start), int(end + 1)):
                             yield i
                     else:
                         yield int(chunk)
