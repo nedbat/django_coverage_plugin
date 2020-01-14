ea1b1019cae34f53aec37e02cb21a6cfd53fe741
Bruno Alla <alla.brunoo@gmail.com>
2020-01-14 12:37:39 +0000

    Add Django 2.2 and 3.0 to the build matrix

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ Framework :: Django :: 1.8
 Framework :: Django :: 1.11
 Framework :: Django :: 2.0
 Framework :: Django :: 2.1
+Framework :: Django :: 2.2
+Framework :: Django :: 3.0
 """
 
 setup(
