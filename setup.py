a1f1d93bccbd495e0d019052a6e411f88f5cbc6f
Ned Batchelder <ned@nedbatchelder.com>
2025-01-27 19:18:51 -0500

    drop Python 3.8

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ Environment :: Console
 Intended Audience :: Developers
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
-Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
