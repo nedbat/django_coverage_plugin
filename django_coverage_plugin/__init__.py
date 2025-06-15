11c54bceead4ceddbe0fa9d68943307689d2ac7e
Ned Batchelder <ned@nedbatchelder.com>
2025-06-15 09:56:29 -0400

    build: remove setup.py and setup.cfg

--- django_coverage_plugin/__init__.py
+++ django_coverage_plugin/__init__.py
@@ -3,7 +3,7 @@
 
 """Django Template Coverage Plugin"""
 
-__version__ = "3.1.1rc1"
+__version__ = "3.1.1rc2"
 
 from .plugin import DjangoTemplatePluginException  # noqa
 from .plugin import DjangoTemplatePlugin
